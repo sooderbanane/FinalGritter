913ea44c439f372b86a467385a447dba661668b4
soba <erik@wildpaner.com>
2025-06-30 14:22:41 +0200

    now it workes with mins

--- managing_sensorData/Analizing.py
+++ managing_sensorData/Analizing.py
@@ -7,9 +7,9 @@ from datetime import datetime
 
 # Hardcoded shit
 DATA_DIR = 'sensor_data'
-CHECK_INTERVAL = 60
+CHECK_INTERVAL = 10
 
-checked_hours = set()  
+checked_mins = set()  
 
 while True: 
     print (f"\n[+] Checking data at {datetime.now()}")
@@ -37,23 +37,23 @@ while True:
 
     # makes it into hourly groups for later analyzing 
     events = pd.concat(all_events)
-    events['hour'] = events['timestamp'].dt.floor('h')
-    hourly_counts = events.groupby('hour').size().reset_index(name='request_count')
+    events['min'] = events['timestamp'].dt.floor('h')
+    minly_counts = events.groupby('min').size().reset_index(name='request_count')
 
 
     # checks for duplicates
-    last_hour = hourly_counts['hour'].max()
+    last_min = minly_counts['min'].max()
 
-    if last_hour not in checked_hours and last_hour < datetime.now().replace(minute=0, second=0, microsecond=0):
-        test_data = hourly_counts[hourly_counts['hour'] == last_hour]
+    if last_min not in checked_mins and last_min < datetime.now().replace(second=0, microsecond=0):
+        test_data = minly_counts[minly_counts['min'] == last_min]
 
         # Checking for amount of data for training
-        if len(hourly_counts) < 5:
-            print(f"Need more hours of data ({len(hourly_counts)}/5). Waiting for more...")
+        if len(minly_counts) < 5:
+            print(f"Need more mins of data ({len(minly_counts)}/5). Waiting for more...")
         # Setting testdata / traindata 
         else:
-            train_data = hourly_counts.iloc[:-1]
-            test_data = hourly_counts.iloc[-1:]
+            train_data = minly_counts.iloc[:-1]
+            test_data = minly_counts.iloc[-1:]
 
             # Actual testing 
             model = IsolationForest(contamination=0.05, random_state=42)
@@ -62,15 +62,15 @@ while True:
 
 
             if prediction[0] == -1:
-                print(f"⚠️ ANOMALY: {test_data['request_count'].values[0]} requests in last hour")
+                print(f"⚠️ ANOMALY: {test_data['request_count'].values[0]} requests in last min")
             else:
-                print(f"✅ Normal: {test_data['request_count'].values[0]} requests in last hour")
+                print(f"✅ Normal: {test_data['request_count'].values[0]} requests in last min")
 
             #writing into extra file 
             with open ('anomalies.csv', 'a') as f :
                 f.write(f"{datetime.now()}, ANOMALY, {test_data['request_count'].values[0]}\n")
 
-            checked_hours.add(last_hour)
+            checked_mins.add(last_min)
 
     time.sleep(CHECK_INTERVAL)
     
